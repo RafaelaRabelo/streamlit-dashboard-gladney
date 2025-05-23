52a57380d480b01fa71bb7783657b2a95bc7e620
Rafaela Bernardes Rabelo <165805309+RafaelaRabelo@users.noreply.github.com>
2025-05-23 11:51:43 -0300

    Add files via upload

---
+++ start_dashboard.bat
@@ -0,0 +1,16 @@
+@echo off
+echo Iniciando Streamlit com Chrome...
+
+:: Caminho até a pasta onde está seu projeto
+cd "C:\Users\rafae\Downloads\Projeto_Gladney_Dashboard"
+
+:: Executa o Streamlit sem abrir aba automaticamente
+start "" streamlit run test_looker.py --server.headless true
+
+:: Aguarda alguns segundos para garantir que o servidor subiu
+timeout /t 5 > nul
+
+:: Abre manualmente no Chrome
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" http://localhost:8501
+
+pause
